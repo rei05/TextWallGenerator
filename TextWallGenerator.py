820fd0205eb81297456c4b36372dfa076719a04a
rei05 <hasegawa0kn@gmail.com>
2023-02-01 23:02:51 +0900

    initial commit

---
+++ TextWallGenerator.py
@@ -0,0 +1,266 @@
+import os
+import re
+import numpy as np
+import csv
+import json
+#from pykakasi import kakasi
+from functions import *
+import unicodedata
+import yaml
+
+#kakasi = kakasi() 
+#kakasi.setMode("H", "a")
+#kakasi.setMode("K", "a")
+#kakasi.setMode("J", "a")
+#kakasi.setMode("r", "Hepburn")
+#conv = kakasi.getConverter()
+
+
+def init_wall(x,y,w,h):
+    wall = {'_time': 0, '_lineIndex': 0, '_type': 0, '_duration': 0, '_width': 0,
+            '_customData': {'_position': [x,y], '_scale': [w,h,0.1], '_animation': {'_scale': [[1,1,1,0]]},
+                            '_fake':True, '_interactable':False, "_noteJumpStartBeatOffset": 0}
+            }
+    return wall
+
+def forced_centering(walls):
+        x_list = np.array([wall['_customData']['_position'][0] for wall in walls['_obstacles']])
+        w_list = np.array([wall['_customData']['_animation']['_scale'][0][0] for wall in walls['_obstacles']])
+        x_offset = -(max(x_list+w_list) + min(x_list))/2
+        for wall in walls['_obstacles']:
+            wall['_customData']['_position'][0] += x_offset
+
+def frameThick(walls, thicc):
+    for wall in walls['_obstacles']:
+        w = wall['_customData']['_animation']['_scale'][0][0]
+        wall['_customData']['_scale'][0] *= 1/thicc
+        wall['_customData']['_scale'][1] *= 1/thicc
+        wall['_customData']['_scale'][2] *= 1/thicc
+        wall['_customData']['_animation']['_scale'][0][0] *= thicc
+        wall['_customData']['_animation']['_scale'][0][1] *= thicc
+        wall['_customData']['_animation']['_scale'][0][2] *= thicc
+        wall['_customData']['_position'][0] -= (1-1/thicc)/2
+
+# scaleとanimatesacleを入れ替え
+def x_adjust(walls):
+    for wall in walls['_obstacles']:
+        w,h,l = wall['_customData']['_scale']
+        wall['_customData']['_animation']['_scale'][0] = [w,h,l,0]
+        wall['_customData']['_scale'] = [1,1,1]
+        wall['_customData']['_position'][0] += (w-1)/2
+
+def setTime(walls,time):
+    for wall in walls['_obstacles']:
+        wall['_time'] = time
+
+def setDuration(walls,duration):
+    for wall in walls['_obstacles']:
+        wall['_duration'] = duration
+
+def setTrack(walls,trackName):
+    for wall in walls['_obstacles']:
+        wall['_customData']['_track'] = trackName
+
+def setDissolve(walls):
+    for wall in walls['_obstacles']:
+        wall['_customData']['_animation']['_dissolve'] = [[0,0],[1,1e-9]]
+
+def setDefinitePosition(walls):
+    for wall in walls['_obstacles']:
+        wall['_customData']['_animation']['_definitePosition'] = [[0,0,0,0]]
+
+def centering(walls,fontDotSize):
+    for wall in walls['_obstacles']:
+        x,y = wall['_customData']['_position']
+        x += 0.5-fontDotSize/2
+        y += 0.5-fontDotSize/2
+        wall['_customData']['_position'] = [x,y]
+
+def resize(walls,charSize,fontDotSize):
+    resizeRate = charSize/fontDotSize
+    for wall in walls['_obstacles']:
+        x,y = wall['_customData']['_position']
+        w,h,l = wall['_customData']['_scale']
+        x = (x+0.5-fontDotSize/2)*resizeRate
+        y = (y+0.5-fontDotSize/2)*resizeRate
+        w *= resizeRate
+        h *= resizeRate
+        wall['_customData']['_position'] = [x,y]
+        wall['_customData']['_scale'] = [w,h,l]
+
+
+def positionOffset(walls, size, length, char_shift, direction, isSmall, isHalf):
+    for wall in walls['_obstacles']:
+        x,y = wall['_customData']['_position']
+        x += size
+        y += size
+        if direction=='h':
+            x += char_shift - (length/2)*size - 0.1
+            y += 0
+            if isHalf:
+                x += size*0.25
+        elif direction=='v':
+            x += (0.8-size/2)
+            y -= char_shift + size - (length+1)*size
+            if isSmall:
+                y += size*0.2
+        wall['_customData']['_position'] = [x,y]
+
+def make_rotation_sw(walls,name,Angle,Direction,Duration,position,rotation,rotType):
+    text = f'workspace: {name}\n'
+    for wall in walls['_obstacles']:
+        text += '\n0: RotationWall\n' if rotType == 1 else '\n0: RotationWall2\n'
+        w,h,l = wall['_customData']['_scale']
+        text += f'\tScale: [{w},{h},{l}]\n'
+        x,y = wall['_customData']['_position']
+        text += f'\tRadius: {x+w/2}\n'
+        text += f'\tCenter: [{position[0]},{y+h/2+position[1]},{position[2]}]\n'
+        text += f'\tAngle: {Angle}\n'
+        text += f'\tDirection: {Direction}\n'
+        text += f'\tDuration: {Duration}\n'
+    text += '\n0: appendwalls\n'
+    text += f'\tAnimateRotation: [{rotation[0]},{rotation[1]},{rotation[2]},0]\n'
+    text += '\tappendtechnique: 0\n'
+    return text
+
+
+def charType(char):
+    re_hiragana = re.compile(r'^[あ-ん]+$')
+    if re_hiragana.fullmatch(char): return 'hira'
+    re_katakana = re.compile(r'[\u30A1-\u30F4]+')
+    if re_katakana.fullmatch(char): return 'kata'
+    re_kanji = re.compile(r'^[\u4E00-\u9FD0]+$')
+    if re_kanji.fullmatch(char): return 'kanji'
+    return 'char'
+
+def offsetCharSize(cType,size):
+    if cType=='kanji':
+        charSize = size*1.1
+    elif cType=='hira':
+        charSize = size*0.9
+    else:
+        charSize = size
+    return charSize
+
+#def jp2Roman(char):
+#    result = conv.do(char)
+#    if result=='\u300c': result = 'left'
+#    if result=='\u300d': result = 'right'
+#    if result=='/': result = 'slash'
+#    return result
+
+def isSmall(char):
+    smallChars = ['っ','ゃ','ゅ','ょ','ぁ','ぃ','ぅ','ぇ','ぉ','ゎ']
+    smallChars.extend(['ッ','ャ','ュ','ョ','ァ','ィ','ゥ','ェ','ォ','ヮ'])
+    return (char in smallChars)
+
+def isHalf(char):
+    return 'Na' == unicodedata.east_asian_width(char)
+
+def makeWall(char,font,n_dot):
+
+    #文字を画像に変換
+    img,fullwide = char2image(char,font)
+
+    #画像を二値行列に変換
+    matrix = image2matrix(img,fullwide,n_dot)
+
+    #複合長方形領域に分割
+    arrangement = div2rectangle(matrix,fullwide,n_dot)
+    
+    _obstacles = []
+    for (x,y,w,h) in arrangement:
+        wall = init_wall(x,y,w,h)
+        _obstacles.append(wall)
+    dat = {'_obstacles':_obstacles}
+    return dat
+
+
+
+if __name__ == '__main__':
+
+    with open('settings.yaml') as f:
+        settings = yaml.safe_load(f)
+
+    with open('input.csv', encoding='shift_jis', newline='') as f:
+        setting = [row for row in csv.reader(f)][1:]
+
+    customEvents = []
+
+    os.makedirs("generated_files", exist_ok=True)
+
+    for row in setting:
+        text       = row[0]
+        track_name = row[1]
+        t_start    = float(row[2])
+        duration   = float(row[3]) - settings["HJD"]*2
+        direction  = row[4]
+        font       = "fonts/" + row[5]
+        dot_size   = int(row[6])
+        behavior   = row[7]
+        length     = len(text)
+        size       = 1
+        time       = t_start + settings["HJD"]
+        
+        charShift = 1
+        obstacles = {"_obstacles":[]}
+        child_tracks = []
+
+        for i_char, char in enumerate(text):
+            if char==' ': continue
+            child_track_name = f'{track_name}_{i_char}'
+            child_tracks.append(child_track_name)
+
+            walls = makeWall(char,font,dot_size)
+            ini_walls = walls.copy()
+
+            centering(walls,dot_size)
+
+            resize(walls,size,dot_size)
+
+            #if isHalf(char) & (direction == "h"):
+            #    charShift -= size*0.5
+            #if isSmall(char): 
+            #    if direction == "v":
+            #        charShift -= size*0.3
+            #    elif direction == "h":
+            #        charShift -= size*0.15
+            #    else:
+            #        exit('ERROR: Direction設定が不正です。')
+
+            positionOffset(walls, size, length, charShift, direction, isSmall(char), isHalf(char))
+            setTrack(walls, child_track_name)
+            setDuration(walls, duration)
+            setTime(walls, time)
+            if behavior=="stop":
+                setDissolve(walls)
+                setDefinitePosition(walls)
+
+            x_adjust(walls)
+            frameThick(walls, 12)
+
+            #forced_centering(walls)
+
+            obstacles["_obstacles"].extend(walls["_obstacles"])
+
+            charShift += size
+
+            if isSmall(char) & (direction != "v"):
+                charShift -= size*0.15
+
+        with open('generated_files/'+track_name+'.dat','w') as f:
+            dat = json.dumps(obstacles["_obstacles"], indent=4)[6:-2].replace("\n    ","\n")
+            f.write(dat)
+
+        customEvents.append({'_time':0,'_type':'AssignTrackParent',
+                             '_data':{'_parentTrack': track_name,
+                                      '_childrenTracks': child_tracks}
+                            })
+
+    parentTracksData = {'_customData':{'_customEvents':customEvents}}
+    with open('generated_files/parentTracks.dat','w') as f:
+        dat = json.dumps(parentTracksData["_customData"]["_customEvents"], indent=4)[6:-2].replace("\n    ","\n")
+        f.write(dat)
+
+    print("Completed!")
+    os.system('PAUSE')
---
+++ functions.py
@@ -0,0 +1,95 @@
+from PIL import Image,ImageDraw,ImageFont
+import numpy as np
+import unicodedata
+
+fontsize = 256 #フォントサイズ
+
+#文字を画像に変換
+def char2image(char, ttfontname):
+    
+    #全角/半角判定
+    fullwide = unicodedata.east_asian_width(char) in 'FWA'
+    canvasSize = (fontsize,fontsize) if fullwide else (int(fontsize/2),fontsize)
+
+    # 文字を描く画像の作成
+    img  = Image.new('RGB', canvasSize, (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+
+    # 用意した画像に文字列を描く
+    font = ImageFont.truetype(ttfontname, fontsize)
+    draw.textsize(char,font=font)
+    textTopLeft = (0,0)
+    draw.text(textTopLeft, char, fill=(0,0,0), font=font)
+
+    return img,fullwide
+
+
+def image2matrix(img,fullwide,n_dot):
+    
+    dotsize = fontsize/n_dot #ドットサイズ
+
+    #全角/半角判定
+    _n_dot = n_dot if fullwide else int(n_dot/2)
+    
+    matrix = []
+    for iy in range(n_dot):
+        row = []
+        for ix in range(_n_dot):
+            c_x = int(ix*dotsize+(dotsize/2))
+            c_y = int(iy*dotsize+(dotsize/2))
+            value = np.array(img)[c_y][c_x][0]
+            row.append(1 if value<120 else 0)
+        matrix.append(row)
+        
+    return matrix
+
+
+def div2rectangle(matrix,fullwide,n_dot):
+    
+    #全角/半角判定
+    _n_dot = n_dot if fullwide else int(n_dot/2)
+
+    matrix = (np.array(matrix))
+    arrangement = []
+    while True:
+        h_lines = []
+        v_lines = []
+        # Horizontal lines
+        for i in range(n_dot):
+            start_j = None
+            for j in range(_n_dot):
+                if matrix[i, j] and start_j is None:
+                    start_j = j
+                if not matrix[i, j] and start_j is not None:
+                    h_lines.append((j - start_j, i, start_j))
+                    start_j = None
+            if start_j is not None:
+                h_lines.append((_n_dot - start_j, i, start_j))
+        # Vertical lines
+        for j in range(_n_dot):
+            start_i = None
+            for i in range(n_dot):
+                if matrix[i, j] and start_i is None:
+                    start_i = i
+                if not matrix[i, j] and start_i is not None:
+                    v_lines.append((i - start_i, start_i, j))
+                    start_i = None
+            if start_i is not None:
+                v_lines.append((n_dot - start_i, start_i, j))
+        # Choose the longest line, add it and clear the pixels
+        h_lines.sort(key=lambda x: x[0], reverse=True)
+        v_lines.sort(key=lambda x: x[0], reverse=True)
+        if not h_lines and not v_lines:
+            break
+        elif not h_lines or v_lines[0][0] >= h_lines[0][0]:
+            h, i, j = v_lines[0]
+            arrangement.append((j, n_dot - i - h, 1, h))
+            for r in range(i, i + h):
+                matrix[r, j] = False
+        else:
+            w, i, j = h_lines[0]
+            arrangement.append((j, n_dot - i - 1, w, 1))
+            for c in range(j, j + w):
+                matrix[i, c] = False
+
+    return arrangement
